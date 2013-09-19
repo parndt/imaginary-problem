8003d3d7b1368879847dbd23f6fab98e7c548f77
Daniel Fone <daniel@fone.net.nz>
2013-09-19 13:51:59 +1200

    Make check work

--- problem.rb
+++ problem.rb
@@ -5,6 +5,17 @@ end
 
 # -- Test "suite"
 
+def check(values, &test)
+  values.each do |input, expected|
+    actual = test.call input
+    if actual == expected
+      puts "#{input.inspect} => #{actual.inspect}"
+    else
+      puts "Expected #{input.inspect} => #{expected.inspect}: got #{actual.inspect}"
+    end
+  end
+end
+
 tests = {
    1    => -1,
    0.1  => -0.1,
