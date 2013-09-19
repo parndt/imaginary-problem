08e1f2e592d77482cfb5fef8e036185ceddd640e
Daniel Fone <daniel@fone.net.nz>
2013-09-19 13:16:58 +1200

    Example tests

--- problem.rb
+++ problem.rb
@@ -2,3 +2,17 @@
 def f(n)
   # define f so that f(f(n)) == -n
 end
+
+# -- Test "suite"
+
+tests = {
+   1    => -1,
+   0.1  => -0.1,
+  -0.01 =>  0.01,
+   0    =>  0,
+   nil  =>  nil,
+}
+
+check tests do |n|
+  f(f(n))
+end
